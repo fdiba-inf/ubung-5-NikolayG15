dafe9ca30672f3420e811a754b738200fb0fb87b
NikolayG15 <>
2021-11-18 08:25:33 +0000

    Update

--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -7,25 +7,29 @@ public class UniqueNumbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] uniqueNumbers = new int[numbers.length];
-        int nextUniqueIndex = 0;
-
+        System.out.print("Enter number count: ");
+        int numberCount = input.nextInt();
+        int[] numbers = new int[numberCount];
+        System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
-          boolean found = false;
-          for (int i = 0; i < uniqueNumbers.length; i++) {
-            if (numbers[index] == uniqueNumbers[i]) {
+            numbers[index] = input.nextInt();
+        }
+        int[] uniqueNumbers = new int[numbers.length];
+        int nextUniqueIndex = 0; 
+        for ( int index = 0; index < numbers.length; index++) {
+          boolean found = false; 
+          for (int i = 0; i < uniqueNumbers.length; i++){
+            if (numbers[index]== uniqueNumbers [i]){
               found = true;
               break;
             }
           }
-          if (!found) {
-            uniqueNumbers[nextUniqueIndex] = numbers[index];
-            nextUniqueIndex++;
+           if (!found){
+             uniqueNumbers[nextUniqueIndex]= numbers[index];
+             nextUniqueIndex++;
           }
         }
-
-        // Find unique numbers in numbers
-
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
+        System.out.println("Unique numbers: " + uniqueNumbersAsString);
     }
 }
